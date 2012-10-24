d35eb0458727a216a86dfa53c24994b6df338730
Lee Bailey <baileyl3@tcnj.edu>
2012-10-23 21:07:17 -0400

    First Push
    Initial setup with members and music model

---
+++ LyricsGame/Models/MusicModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LyricsGame.Models
+{
+    public class Music
+    {
+            public int ID { get; set; }
+            public string Title { get; set; }
+            public string Artist { get; set; }
+            public string Genre { get; set; }
+            public string AllLyrics { get; set; }
+            public string FilePath { get; set; }
+    }
+
+    public class MusicDBContext : DbContext
+    {
+        public DbSet<Music> Movies { get; set; }
+    }
+}
